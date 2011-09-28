9dd39d7695d8f96092c774a436204744d53fb40b
kimura-y <kimura-y@swlab.cs.okayama-u.ac.jp>
2011-09-28 16:31:33 +0900

    Rails3.1環境構築

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nomcolle::Application.config.secret_token = '76a8d57a362cb09d22ab1003db818ddc78da720c6c49ae27c84c13491e7144bc88b8ee15b51596d75aca79138a86e91132bbb95cff0983fa50ceef8c6ea33ab8'
