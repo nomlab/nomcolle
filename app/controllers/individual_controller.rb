f7211948852f9559ff65e8bd7c7aa54400011b8a
Hideto Yoshii <blue_snow_sea_world@yahoo.co.jp>
2012-08-04 13:32:12 +0900

    個人ページの改良

--- app/controllers/individual_controller.rb
+++ app/controllers/individual_controller.rb
@@ -1,5 +1,8 @@
 class IndividualController < ApplicationController
   def index
+    @histories = History.where(:user_id => User.current)
+    @reviews = Review.where(:user_id => User.current)
+    @total_point = History.my_total_point
   end
 
 end
