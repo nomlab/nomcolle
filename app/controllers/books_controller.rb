33cadf4ef2d93083b57641a64eae3c15d04b8267
Hideto Yoshii <blue_snow_sea_world@yahoo.co.jp>
2012-08-08 13:54:30 +0900

    paginte機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.paginate(:page => params[:page], :per_page => 10)  
 
     respond_to do |format|
       format.html # index.html.erb
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Nomcolle::Application.initialize!
+
+require 'will_paginate'
