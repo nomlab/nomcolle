2600e49048314dceb527dd70ca13ca47fa63e969
Hideto Yoshii <blue_snow_sea_world@yahoo.co.jp>
2012-08-09 17:06:13 +0900

    リストの並び順の変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.paginate(:page => params[:page], :per_page => 10)  
+    @books = Book.paginate(:page => params[:page], :per_page => 10, :order => 'created_at DESC')  
 
     respond_to do |format|
       format.html # index.html.erb
