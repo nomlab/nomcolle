c33506a3dd478c43df5b72017d178cc4135929b2
Takuya Okada <pitipitiunsyumikan@gmail.com>
2013-11-21 17:46:16 +0900

    Create User at seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = User.create(name: 'guest', login_name:'guest', password: 'guest')
-bowling_match = BowlingMatch.create(name: 'test match', start_time: Time.now, end_time: Time.now, filename: 'sample.xls', steward: '106')
-room = Room.create(name: '106')
-team = BowlingTeam.create(name: 'test team', bowling_match_id: bowling_match.id, room_id: room.id, total_score: 500, average_score: 250)
-BowlingTeamMembership.create(user_id: user.id, bowling_team_id: team.id)
-BowlingScore.create(score: 300, user_id: user.id, bowling_match_id: bowling_match.id, bowling_team_id: team.id, game_number: 1)
-BowlingScore.create(score: 200, user_id: user.id, bowling_match_id: bowling_match.id, bowling_team_id: team.id, game_number: 2)
+User.create(name: "guest", login_name:"guest", password: "guest")
+
+
