6b26847719d1632a7a2855f20c2874719da5bcd2
Hideto Yoshii <blue.snow.sea.world@gmail.com>
2013-11-26 16:39:30 +0900

    Change to a boolean return value

--- app/models/bowling_score.rb
+++ app/models/bowling_score.rb
@@ -6,10 +6,12 @@ class BowlingScore < ActiveRecord::Base
   validates :score, :numericality => {:only_integer => true, :less_than_or_equal_to => 300, :greater_than_or_equal_to => 0}
 
   def self.create_by_game_number(user_id, bowling_match_id, game_number = 1)
+    result = true
     (1..game_number).each do |num|
       if self.find_by_user_id_and_bowling_match_id_and_game_number(user_id, bowling_match_id, num) == nil
-        self.create(:user_id => user_id, :bowling_match_id => bowling_match_id, :game_number => num)
+        result &= self.create(:user_id => user_id, :bowling_match_id => bowling_match_id, :game_number => num, :score => 0)
       end
     end
+    result
   end
 end
