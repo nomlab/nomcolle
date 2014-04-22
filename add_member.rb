c89d9c28e5c6ff2e3d084a6d3ac0c3ec647d6dc7
Takuya Okada <pitipitiunsyumikan@gmail.com>
2014-04-22 18:53:11 +0900

    Create a script to add member

---
+++ add_member.rb
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+print "以下の例に従って入力してください．\nName    : 岡大 太郎\nUserName: okadai\nPassword: okadaI\n\n"
+print "Name    : "; name = gets.chomp
+print "UserName: "; user = gets.chomp
+print "Password: "; pass = gets.chomp
+User.create(name: "#{name}", login_name:"#{login_name}", password: "#{pass}")
